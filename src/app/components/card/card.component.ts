c0384acb6ab7f59b5e655615e4420ea1ec405ee7
matheus <matheuszxx1@outlook.com>
2023-10-13 11:57:50 -0300

    repassando parametros de components pais para filhos

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+	//para linkar essa propriedade no HTML do card faremos um DataBiding na tag img no HTML
+	@Input()
+	gameCover:string=""
+	@Input()
+	gameLabel:string=""
+	@Input()
+	gameType:string=""
+	@Input()
+	gamePrice:string=""
 
   constructor() { }
 
